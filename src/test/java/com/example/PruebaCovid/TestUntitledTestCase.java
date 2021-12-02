e52f2cb5b6bb083c11d31590e058162b225ba0d0
haalbrec <73978888+haalbrec@users.noreply.github.com>
2021-12-01 18:33:38 -0600

    Add files via upload

---
+++ src/test/java/com/example/PruebaCovid/TestUntitledTestCase.java
@@ -0,0 +1,91 @@
+package com.example.PruebaCovid;
+
+import java.util.regex.Pattern;
+import java.util.concurrent.TimeUnit;
+import org.junit.*;
+import static org.junit.Assert.*;
+import static org.hamcrest.CoreMatchers.*;
+import org.openqa.selenium.*;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.Select;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+
+import org.apache.commons.io.FileUtils;
+import java.io.File;
+
+public class TestUntitledTestCase {
+  private WebDriver driver;
+  private String baseUrl;
+  private boolean acceptNextAlert = true;
+  private StringBuffer verificationErrors = new StringBuffer();
+  JavascriptExecutor js;
+  @Before
+  public void setUp() throws Exception {
+	WebDriverManager.chromedriver().setup();
+    driver = new ChromeDriver();
+    baseUrl = "https://www.google.com/";
+    driver.manage().timeouts().implicitlyWait(60, TimeUnit.SECONDS);
+    js = (JavascriptExecutor) driver;
+  }
+
+  @Test
+  public void testUntitledTestCase() throws Exception {
+    driver.get("https://www.google.com/search?q=coronavirus&rlz=1C1CHBF_esMX971MX971&oq=corona&aqs=chrome.0.0i433i457i512j69i57j0i402l2j0i512j46i131i433i512j0i131i433i512l3.2616j0j7&sourceid=chrome&ie=UTF-8");
+    driver.findElement(By.xpath("//div[@id='kp-wp-tab-overview']/div[3]/div/div[2]/div/div/div/div/div/div/div/div/a/h3")).click();
+    String textoEjecucion = driver.getTitle();
+    
+    pause(30000);
+   
+    assertEquals("Coronavirus – gob.mx", driver.getTitle());
+  }
+
+  @After
+  public void tearDown() throws Exception {
+    driver.quit();
+    String verificationErrorString = verificationErrors.toString();
+    if (!"".equals(verificationErrorString)) {
+      fail(verificationErrorString);
+    }
+  }
+
+  private boolean isElementPresent(By by) {
+    try {
+      driver.findElement(by);
+      return true;
+    } catch (NoSuchElementException e) {
+      return false;
+    }
+  }
+
+  private boolean isAlertPresent() {
+    try {
+      driver.switchTo().alert();
+      return true;
+    } catch (NoAlertPresentException e) {
+      return false;
+    }
+  }
+
+  private String closeAlertAndGetItsText() {
+    try {
+      Alert alert = driver.switchTo().alert();
+      String alertText = alert.getText();
+      if (acceptNextAlert) {
+        alert.accept();
+      } else {
+        alert.dismiss();
+      }
+      return alertText;
+    } finally {
+      acceptNextAlert = true;
+    }
+  }
+  private void pause(long mils) {
+	  try {
+		  Thread.sleep(mils);
+	  }catch(Exception e){
+		  e.printStackTrace();
+	  }
+  }
+}
